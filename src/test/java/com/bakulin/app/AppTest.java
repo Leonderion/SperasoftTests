01725e1c35e64f3dd0a1e0b78917baa90110a506
Владислав Бакулин <vladislav.bakulin@eaeconsult.ru>
2019-10-12 17:23:58 +0400

    Отключилл плагин чтобы можно было пользоваться breakpoints, добавил обработку ожидания элемента

--- src/test/java/com/bakulin/app/AppTest.java
+++ src/test/java/com/bakulin/app/AppTest.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -20,23 +23,27 @@ public class AppTest {
         ChromeOptions option = new ChromeOptions();
         option.addArguments("--start-maximized");
         // Create a new instance of the Firefox driver
+
         driver = new ChromeDriver(option);
     }
 
     @Test // Marking this method as part of the test
-    public void gotoSeleniumWikiPage() {
-        // Go to the Wikipedia home page
-        driver.get("https://en.wikipedia.org/");
-        // Find the text input element by its id and type "Selenium"
-        driver.findElement(By.id("searchInput")).sendKeys("Selenium");
-        // Click search button
-        driver.findElement(By.id("searchButton")).click();
-        // Get text from header of the Selenium page
-        String header = driver.findElement(By.id("firstHeading")).getText();
-        // Verify that header equals "Selenium"
-        Assert.assertEquals(header, "Selenium");
-    }
+    public void gotoSite() {
+        Wait<WebDriver> wait = new WebDriverWait(driver, 5, 1000);
+        // Go to site
+        driver.get("http://automationpractice.com/");
 
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("a.login")));
+        driver.findElement(By.cssSelector("a.login")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h1.page-heading")));
+        String header = driver.findElement(By.cssSelector("h1.page-heading")).getText();
+
+        // Verify that header equals "AUTHENTICATION"
+        Assert.assertEquals(header, "AUTHENTICATION");
+        //Thread.sleep(10000);
+    }
+/*
     @Test // Marking this method as part of the test
     public void gotoSeleniumWikiPageFailure() {
         // Go to the Wikipedia home page
@@ -50,7 +57,7 @@ public class AppTest {
         // Verify that header equals "Selenium WebDriver"
         Assert.assertEquals(header, "Selenium Webdriver");
     }
-
+*/
     @AfterClass // Runs this method after all the test methods in the current class have been run
     public void tearDown() {
         // Close all browser windows and safely end the session
