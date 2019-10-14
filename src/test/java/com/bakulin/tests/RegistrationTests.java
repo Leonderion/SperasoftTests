1a6538fd59c150cf7c3f5cc1ea480210a07306f8
Leonderion <44430223+Leonderion@users.noreply.github.com>
2019-10-14 04:54:49 +0400

    Добавил testng.xml

--- src/test/java/com/bakulin/tests/RegistrationTests.java
+++ src/test/java/com/bakulin/tests/RegistrationTests.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 
 public class RegistrationTests extends TestBase {
 
-    @Test (dataProvider = "data-provider", dataProviderClass = DataProviders.class)
+    @Test (dataProvider = "data-provider", dataProviderClass = DataProviders.class, testName = "AllField")
     public void createAnAccountAllField(User user) throws InterruptedException {
 
         app.registerNewUserAllField(user);
@@ -14,7 +14,7 @@ public class RegistrationTests extends TestBase {
         Assert.assertEquals(app.getHeaderAfterLogon(), "MY ACCOUNT");
     }
 
-    @Test (dataProvider = "data-provider", dataProviderClass = DataProviders.class)
+    @Test (dataProvider = "data-provider", dataProviderClass = DataProviders.class, testName = "RequiredField")
     public void createAnAccountRequiredFieldsOnly(User user) throws InterruptedException {
 
         app.registerNewUserAllFieldRequiredFieldsOnly(user);
